bbbb09a9e2688cf690dec36fbb868efa8c1b4626
GulinAlexey <gulin-2001@mail.ru>
2025-08-05 21:32:30 +0700

    Заготовка проекта для сборки через cmake

---
+++ Check_Set_in_Document.cpp
@@ -0,0 +1,12 @@
+﻿// Check_Set_in_Document.cpp: определяет точку входа для приложения.
+//
+
+#include "Check_Set_in_Document.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
